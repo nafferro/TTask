fdda5c61bcbc2776196cff53be938a441b941a65
Nuno Ferro <nuno.ferro@puregym.com>
2022-10-31 15:25:47 +0000

    Initial Commit

---
+++ TTask/TTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  TTaskApp.swift
+//  TTask
+//
+//  Created by Nuno Ferro on 31/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct TTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
